ad26a5d8f94591768346d361e435132753422ec2
Milot Mirdita <milot@mirdita.de>
2017-08-02 14:25:50 +0200

    Just do it single threaded for now

--- taxa2lca.go
+++ taxa2lca.go
@@ -4,17 +4,16 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
-	"github.com/milot-mirdita/mmseqs-web-backend/dbreader"
 	"log"
 	"os"
-	"runtime"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/milot-mirdita/mmseqs-web-backend/dbreader"
 )
 
 var (
-	procsflag  int
 	nodesflag  string
 	namesflag  string
 	taxdbflag  string
@@ -29,8 +28,7 @@ func init() {
 	flag.StringVar(&namesflag, "names", "names.dmp", "names.dmp file of taxonomy")
 	flag.StringVar(&taxdbflag, "taxid database", "taxons_db", "Input database name")
 	flag.StringVar(&taxidxflag, "taxid index", "taxons_db.index", "Input index name")
-	flag.StringVar(&outflag, "Output files", "taxa.tsv", "Output tsv files")
-	flag.IntVar(&procsflag, "nprocs", 4, "Number of cpus for multithreading [optional]")
+	flag.StringVar(&outflag, "output file", "taxa.tsv", "Output tsv file")
 	flag.StringVar(&taxlevel, "levels", "", "Desired LCA taxonomical levels [optional]")
 	flag.BoolVar(&helpflag, "help", false, "Print USAGE and exits")
 	flag.Parse()
@@ -41,76 +39,6 @@ func init() {
 		flag.Usage()
 		os.Exit(0)
 	}
-
-	runtime.GOMAXPROCS(procsflag)
-}
-
-type Job struct {
-	Rank  int64
-	Start int64
-	Size  int64
-}
-
-func lca(input <-chan Job, taxDB *Taxonomy, reader *dbreader.Reader, levs [][]byte, outbase string) {
-
-	for {
-		select {
-		case job, ok := <-input:
-			if ok {
-				f, err := os.Create(outbase + "." + strconv.FormatInt(job.Rank, 10))
-				if err != nil {
-					log.Panic("Could not create output file!")
-				}
-				defer f.Close()
-
-				log.Printf("Computing split from %d to %d", job.Start, job.Start+job.Size)
-				for i := job.Start; i < job.Start+job.Size; i++ {
-					data := reader.Data(i)
-					split := strings.Split(data, "\n")
-					taxa := make([]int, len(split))
-
-					query := strconv.FormatUint(uint64(reader.Key(i)), 10)
-					for i, e := range strings.Split(data, "\n") {
-						taxon, _ := strconv.ParseInt(e, 10, 64)
-						taxa[i] = int(taxon)
-					}
-
-					var atLevs [][]byte
-					var allLevs []byte
-					lcaNode, err := taxDB.LCA(taxa...)
-					if err != nil {
-						atLevs = make([][]byte, 1)
-						atLevs[0] = Unknown
-					} else {
-						if len(levs[0]) != 0 {
-							atLevs = taxDB.AtLevels(lcaNode, levs...)
-						}
-					}
-					allLevs = bytes.Join(atLevs, []byte{';'})
-					f.WriteString(fmt.Sprintf("%s\t%s\t%s\t%s\n", query, lcaNode.Name, lcaNode.Taxon, allLevs))
-				}
-			} else {
-				return
-			}
-		default:
-		}
-	}
-}
-
-func decomposeDomain(domain_size, world_rank, world_size int64) Job {
-	if world_size > domain_size {
-		return Job{world_rank, 0, 0}
-	}
-
-	subdomain_start := domain_size / world_size * world_rank
-	subdomain_size := domain_size / world_size
-
-	if world_rank == world_size-1 {
-		subdomain_size += domain_size % world_size
-	}
-
-	return Job{world_rank, subdomain_start, subdomain_size}
-
 }
 
 func main() {
@@ -126,19 +54,44 @@ func main() {
 	reader := dbreader.Reader{}
 	reader.Make(taxdbflag, taxidxflag)
 
-	done := make(chan bool)
-	jobs := make(chan Job)
+	defer reader.Delete()
 
-	go func() {
-		for i := 0; i < procsflag; i++ {
-			jobs <- decomposeDomain(reader.Size(), int64(i), int64(procsflag))
+	f, err := os.Create(outflag)
+	if err != nil {
+		log.Panic("Could not create output file!")
+	}
+	defer f.Close()
+
+	var i int64
+	for i = 0; i < reader.Size(); i++ {
+		data := reader.Data(i)
+		if data == "" {
+			continue
 		}
-		done <- true
-	}()
-	go lca(jobs, taxDB, &reader, levs, outflag)
-	<-done
 
-	reader.Delete()
+		split := strings.Split(data, "\n")
+		taxa := make([]int, len(split))
+
+		query := strconv.FormatUint(uint64(reader.Key(i)), 10)
+		for i, e := range strings.Split(data, "\n") {
+			taxon, _ := strconv.ParseInt(e, 10, 64)
+			taxa[i] = int(taxon)
+		}
+
+		var atLevs [][]byte
+		var allLevs []byte
+		lcaNode, err := taxDB.LCA(taxa...)
+		if err != nil {
+			atLevs = make([][]byte, 1)
+			atLevs[0] = Unknown
+		} else {
+			if len(levs[0]) != 0 {
+				atLevs = taxDB.AtLevels(lcaNode, levs...)
+			}
+		}
+		allLevs = bytes.Join(atLevs, []byte{';'})
+		f.WriteString(fmt.Sprintf("%s\t%d\t%s\t%s\t%s\n", query, lcaNode.Taxid, lcaNode.Name, lcaNode.Taxon, allLevs))
+	}
 
 	t2 := time.Now()
 	dur := t2.Sub(t1)
